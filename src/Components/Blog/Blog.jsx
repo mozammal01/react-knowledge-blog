e85a8ba68ffc32c5af0439fc8fae3811bfe02ad0
Mozammal Haq <mozammalhaq01@gmail.com>
2024-03-11 01:00:04 +0600

    blog section almost doen

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,12 +1,47 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
+  const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
+  console.log(blog);
   return (
-    <div>
-      <h4>Its Showtime</h4>
-      <div className="w-80">
-        <img src={blog.cover} alt="" />
+    <div className='m-5 border-2 border-gray-600'>
+
+      <div className="">
+        <img src={cover} alt="" />
       </div>
+
+      <div className='flex justify-between items-center font-semibold'>
+
+        <div className='p-3 flex items-center'>
+
+          <div className='m-2'>
+            <img className='w-16' src={author_img} alt={author} />
+          </div>
+
+          <div>
+            <span className='font-bold text-xl'>{author}</span>
+            <p className='text-gray-600'>{posted_date}</p>
+          </div>
+
+        </div>
+
+        <div className='p-3 text-gray-600'>
+          <p>{reading_time} min read</p>
+        </div>
+
+      </div>
+
+      <div>
+        <p className='font-semibold text-2xl m-2'>{title}</p>
+      </div>
+
+      <div className='flex'>
+
+        <p className=' p-3 text-gray-600'>{hashtags[0]}</p>
+        <p className=' p-3 text-gray-600'>{hashtags[1]}</p>
+        <p className=' p-3 text-gray-600'>{hashtags[2]}</p>
+      </div>
+
     </div>
   );
 };
--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,6 +1,6 @@
 const Bookmarks = () => {
   return (
-    <div className="md:w-1/3">
+    <div className="md:w-1/3 border-2 border-red-300 m-3 p-5">
       <p>Bookmarks</p>
     </div>
   );
